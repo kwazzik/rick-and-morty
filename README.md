d3d78b1b5370787219dcc3266c5e6933f7d70ff6
chtot <mishakwazzi@gmail.com>
2025-12-10 03:50:38 +0200

    Add PostgreSQL support and setup .env file

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 - Create venv: `Python -m venv venv`
 - Activate it: `source venv/bin/activate`
 - Install requirements: `pip install -r requirements.txt`
+- Create new Postgres DB & User
+- Copy .env.sample -> .env and populate with all required data
 - Run migrations: `python manage.py migrate`
 - Run celery for tasks handling:
 - Run celery beat for task scheduling
--- rick_and_morty/settings.py
+++ rick_and_morty/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8hxpqnkp!*4*r@h$kh#u^&_l0u!&_cp+a6o@9#xnb*z0xr%oi_"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,11 +84,11 @@ WSGI_APPLICATION = "rick_and_morty.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "rick_and_morty",
-        "USER": "rick_and_morty_user",
-        "PASSWORD": "admin123",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
@@ -137,8 +142,8 @@ SPECTACULAR_SETTINGS = {
     "SERVE_INCLUDE_SHEMA": False,
 }
 
-CELERY_BROKER_REDIS_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_BROKER_REDIS_URL = os.getenv("CELERY_BROKER_REDIS_URL")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
 CELERY_TIMEZONE = "Europe/Kyiv"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
